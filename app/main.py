00effe5b734d501079c2fabc351b165e20b544c2
DESKTOP-FR144OA\Anubis <adamas87@gmail.com>
2025-10-20 12:11:20 +0800

    'Fix'

--- app/main.py
+++ app/main.py
@@ -6,10 +6,10 @@ def copy_file(command: str) -> None:
     if len(parts) != 3 or parts[0] != "cp":
         return
 
-    file1, file2 = parts[1], parts[2]
-    if file1 == file2 or not os.path.exists(file1):
+    source_file_name, destination_file_name = parts[1], parts[2]
+    if source_file_name == destination_file_name or not os.path.exists(source_file_name):
         return
 
-    with open(file1, "r") as file_in, open(file2, "w") as file_out:
+    with open(source_file_name, "r") as file_in, open(destination_file_name, "w") as file_out:
         for line in file_in:
             file_out.write(line)
